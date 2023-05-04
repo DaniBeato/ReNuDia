01c2e7560e1990797b7cf724dbdc2f7f23b8c9ef
Daniel <danielbeatoramirez@gmail.com>
2023-05-04 10:53:26 -0300

    vista de mensajes casi lista, y haciendo auth del backend

--- backend/main/resources/nutritional_record_resource.py
+++ backend/main/resources/nutritional_record_resource.py
@@ -3,6 +3,8 @@ from flask import request
 from main.repositories.nutritional_record_repository import NutritionalRecordRepository
 from main.maps.nutritional_record_schema import NutritionalRecordSchema
 from .. import db
+from main.auth.decoradores import login_required
+
 
 nutritional_record_repository = NutritionalRecordRepository()
 nutritional_record_schema = NutritionalRecordSchema()
@@ -10,10 +12,13 @@ nutritional_record_schema = NutritionalRecordSchema()
 
 
 class NutritionalRecordsResource(Resource):
+
+    @login_required
     def get(self):
         nutritional_records = nutritional_record_repository.get_all()
         return nutritional_record_schema.dump(nutritional_records, many = True)
 
+    @login_required
     def post(self):
         session = db.session.session_factory()
         nutritional_record = nutritional_record_schema.load(request.get_json(), session=session)
@@ -24,17 +29,20 @@ class NutritionalRecordsResource(Resource):
 
 
 class NutritionalRecordResource(Resource):
+
+    @login_required
     def get(self,id):
        nutritional_record = nutritional_record_repository.get_by_id(id)
        return nutritional_record_schema.dump(nutritional_record)
 
 
-
+    @login_required
     def put(self, id):
         nutritional_record = nutritional_record_repository.update(id, request.get_json().items())
         return nutritional_record_schema.dump(nutritional_record)
 
 
+    @login_required
     def delete(self, id):
         nutritional_record_repository.delete(id)
         return 'Ficha Nutricional Eliminada'
